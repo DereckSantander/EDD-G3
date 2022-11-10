f275f164336962fea936496ead5878e076e05455
mbravop <mbravop03@gmail.com>
2022-11-10 01:04:54 -0500

    Vista Principal y Vista Juego

--- ProyectoEDDG3/src/main/java/grupo3/proyectoeddg3/PrimaryController.java
+++ ProyectoEDDG3/src/main/java/grupo3/proyectoeddg3/PrimaryController.java
@@ -2,11 +2,44 @@ package grupo3.proyectoeddg3;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 
 public class PrimaryController {
-
+    
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    private Button btnTusJuegos;
+    @FXML
+    private Button btnCarrito;
+    @FXML
+    private Button btnCarrIzq;
+    @FXML
+    private HBox hbCarrusel;
+    @FXML
+    private Button btnCarrDer;
+    @FXML
+    private HBox hbGeneros;
+    @FXML
+    private ImageView imv1;
+    @FXML
+    private Label lbl1;
+    @FXML
+    private ImageView imv2;
+    @FXML
+    private Label lbl2;
+    @FXML
+    private ImageView imv3;
+    @FXML
+    private Label lbl3;
+    @FXML
+    private ImageView imv4;
+    @FXML
+    private Label lbl4;
+    @FXML
+    private Button btnGenUp;
+    @FXML
+    private Button btnGenDown;
+    
 }
--- ProyectoEDDG3/src/main/java/grupo3/proyectoeddg3/SecondaryController.java
+++ ProyectoEDDG3/src/main/java/grupo3/proyectoeddg3/SecondaryController.java
@@ -2,8 +2,26 @@ package grupo3.proyectoeddg3;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class SecondaryController {
+    
+    @FXML
+    private Button btnVolver;
+    @FXML
+    private ImageView imvJuego;
+    @FXML
+    private Label lblTituloJuego;
+    @FXML
+    private Label lblDesarrolladores;
+    @FXML
+    private Label lblFechaLanz;
+    @FXML
+    private Label lblDescripcionJuego;
+    @FXML
+    private Label lblPrecioJuego;
 
     @FXML
     private void switchToPrimary() throws IOException {
