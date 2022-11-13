4278be8411ccb8495d3c52a4facdf70179cf4581
mbravop <mbravop03@gmail.com>
2022-11-12 22:31:27 -0500

    Controlador Primario

--- ProyectoEDDG3/src/main/java/grupo3/proyectoeddg3/PrimaryController.java
+++ ProyectoEDDG3/src/main/java/grupo3/proyectoeddg3/PrimaryController.java
@@ -4,42 +4,76 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 public class PrimaryController {
     
     @FXML
-    private Button btnTusJuegos;
-    @FXML
-    private Button btnCarrito;
+    private Button btnCarrDer;
+
     @FXML
     private Button btnCarrIzq;
+
     @FXML
-    private HBox hbCarrusel;
-    @FXML
-    private Button btnCarrDer;
-    @FXML
-    private HBox hbGeneros;
-    @FXML
-    private ImageView imv1;
-    @FXML
-    private Label lbl1;
-    @FXML
-    private ImageView imv2;
-    @FXML
-    private Label lbl2;
-    @FXML
-    private ImageView imv3;
-    @FXML
-    private Label lbl3;
-    @FXML
-    private ImageView imv4;
-    @FXML
-    private Label lbl4;
-    @FXML
-    private Button btnGenUp;
+    private Button btnCarrito;
+
     @FXML
     private Button btnGenDown;
+
+    @FXML
+    private Button btnGenUp;
+
+    @FXML
+    private Button btnTusJuegos;
+
+    @FXML
+    private HBox hbCarrusel;
+
+    @FXML
+    private HBox hbGeneros;
+
+    @FXML
+    private ImageView imv1;
+
+    @FXML
+    private ImageView imv2;
+
+    @FXML
+    private ImageView imv3;
+
+    @FXML
+    private ImageView imv4;
+
+    @FXML
+    private ImageView imvC1;
+
+    @FXML
+    private ImageView imvC2;
+
+    @FXML
+    private ImageView imvC3;
+
+    @FXML
+    private ImageView imvC4;
+
+    @FXML
+    private Label lbl1;
+
+    @FXML
+    private Label lbl2;
+
+    @FXML
+    private Label lbl3;
+
+    @FXML
+    private Label lbl4;
+
+    @FXML
+    private TextField txtFecha;
+
+    @FXML
+    private TextField txtTitulo;
     
 }
