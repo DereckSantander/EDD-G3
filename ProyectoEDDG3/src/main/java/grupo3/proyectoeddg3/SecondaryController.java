d9af54770e69bd1fb79382de2c1d719a13531ec8
mbravop <mbravop03@gmail.com>
2022-11-26 11:24:25 -0500

    Vista juego completa

--- ProyectoEDDG3/src/main/java/grupo3/proyectoeddg3/SecondaryController.java
+++ ProyectoEDDG3/src/main/java/grupo3/proyectoeddg3/SecondaryController.java
@@ -3,25 +3,80 @@ package grupo3.proyectoeddg3;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
 public class SecondaryController {
     
     @FXML
+    private Button btnComprar;
+
+    @FXML
+    private Button btnValoracionAbajo;
+
+    @FXML
+    private Button btnValoracionArriba;
+
+    @FXML
     private Button btnVolver;
+
+    @FXML
+    private ComboBox<?> cmbOrdenFeedback;
+
     @FXML
     private ImageView imvJuego;
+
     @FXML
-    private Label lblTituloJuego;
+    private ImageView imvJuego1;
+
+    @FXML
+    private ImageView imvJuego2;
+
     @FXML
     private Label lblDesarrolladores;
-    @FXML
-    private Label lblFechaLanz;
+
     @FXML
     private Label lblDescripcionJuego;
+
+    @FXML
+    private Label lblFechaLanz;
+
+    @FXML
+    private Label lblGeneroJuego;
+
     @FXML
     private Label lblPrecioJuego;
+
+    @FXML
+    private Label lblTexto1;
+
+    @FXML
+    private Label lblTexto2;
+
+    @FXML
+    private Label lblTexto3;
+
+    @FXML
+    private Label lblTituloJuego;
+
+    @FXML
+    private Label lblUsuario1;
+
+    @FXML
+    private Label lblUsuario2;
+
+    @FXML
+    private Label lblUsuario3;
+
+    @FXML
+    private Label lblValoracion1;
+
+    @FXML
+    private Label lblValoracion2;
+
+    @FXML
+    private Label lblValoracion3;
 
     @FXML
     private void switchToPrimary() throws IOException {
@@ -36,6 +91,7 @@ public class SecondaryController {
         lblFechaLanz.setText(PrimaryController.j.getFechaLanzamiento());
         lblDescripcionJuego.setText(PrimaryController.j.getDescripcion());
         lblPrecioJuego.setText('$'+String.valueOf(PrimaryController.j.getPrecio()));
+        lblGeneroJuego.setText("Género: "+PrimaryController.j.getGenero());
         
     }    
     
